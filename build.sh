2dc28583bef327b1aed31918ac8980e1369bb3da
Hakim Hauston <hakim.hauston@gmail.com>
2016-11-24 18:41:49 +0800

    [iOS] all versions: armv7, armv7s, arm64, i386, x86_64
    Use build.sh to build and combine all architectures

--- build.sh
+++ build.sh
@@ -1,4 +1,2 @@
-make -f Makefile-armv7.iphone
-make -f Makefile-armv7s.iphone
-make -f Makefile-arm64.iphone
-lipo -create libfreeimage-iphonesimulator-x86_64.a libfreeimage-iphonesimulator.a libfreeimage-iphone-armv7.a libfreeimage-iphone-armv7s.a libfreeimage-iphone-arm64.a -output libfreeimage-fat.a
+make -f Makefile-all.iphone
+lipo -create libfreeimage-iphonesimulator-i386.a libfreeimage-iphonesimulator-x86_64.a libfreeimage-iphone-armv7.a libfreeimage-iphone-armv7s.a libfreeimage-iphone-arm64.a -output libfreeimage-fat.a
